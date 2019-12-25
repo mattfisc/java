ccefa85bd46aba01ba37104b8715b0579992ea5d
Matthew Fischer <mattfisc@gmail.com>
2019-12-24 17:54:23 -0800

    mining game

---
+++ mining/src/mining/Mining.java
@@ -0,0 +1,20 @@
+/*
+ * By:     Matthew Fischer
+ * Date:   
+ */
+package mining;
+
+/**
+ *
+ * @author Matthew Fischer
+ */
+public class Mining {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[][] map = new arr[3][3];
+    }
+    
+}
---
+++ mining/src/mining/Robot.java
@@ -0,0 +1,24 @@
+/*
+ * By:     Matthew Fischer
+ * Date:   
+ */
+package mining;
+
+/**
+ *
+ * @author Matthew Fischer
+ */
+public class Robot {
+    private int movement = 1;
+    private int time_worked;
+    
+    public Robot(){
+        
+    }
+    
+    public int mining_effieciency(){
+        int direction = 0;
+        
+        return direction;
+    }
+}
